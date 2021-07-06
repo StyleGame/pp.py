491646eef0adff1215198809d639347d048a80fb
StyleGame <69700134+StyleGame@users.noreply.github.com>
2021-07-06 19:50:03 +0200

    Create US.py

---
+++ US.py
@@ -0,0 +1,244 @@
+import requests
+import requests
+import sys,os
+os.system("clear")
+
+B ='\033[1;92m'
+X ='\033[1;91m'
+re = requests.get("https://pastebin.com/raw/AZWmw69E")
+p = "TOOLS USER INSTA"
+if p == "" :
+  sys.exit()
+if p in str(re.text):
+  print("you Are Active The Sidraelezz")
+  os.system('xdg-open https://t.me/SidraTOOLS')
+else:
+  print(X+"الرجـاء اخـذ مفـتاح التـفعيـل من الـمطـور --->> @SS_SS_1")
+  sys.exit()
+os.system("clear")
+import requests
+import uuid
+import sys
+import os
+from random import *
+import colorama 
+from colorama import *
+import time 
+import string 
+from time import sleep
+from user_agent import generate_user_agent
+import random
+import webbrowser
+import requests
+
+#*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*#
+A = "\033[1;91m"
+B = "\033[1;90m"
+C = "\033[1;97m"
+E = "\033[1;92m"
+H = "\033[1;93m"
+K = "\033[1;94m"
+L = "\033[1;95m"
+#*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*#
+Sidra ="""
+\033[1;96m ------------------------
+ \033[1;32m < COD BY SIDRA ELEZZ >
+ \033[1;96m ------------------------
+\033[1;91m ____  _     _
+\033[1;92m/ ___|(_) __| |_ __ __ _
+\033[1;91m\___ \| |/ _` | '__/ _` |
+\033[1;92m ___) | | (_| | | | (_| |
+\033[1;91m|____/|_|\__,_|_|  \__,_|
+           \033[1;93m _____ _     _____
+           \033[1;92m| ____| |   | ____|________
+           \033[1;91m|  _| | |   |  _| |_  /_  /
+           \033[1;92m| |___| |___| |___ / / / /
+           \033[1;91m|_____|_____|_____/___/___|\033[1;92m
+           
+         
+\033[1;97m--------------------------------------------------
+\033[1;95m
+ AUTHOR     : SIDRA ELEZZ
+ Telegram   : TERMUX TOOLS
+ YOUTUBE    : TERMUX TOOLS
+ GITHUB     : GITHUB.COM/SIDRA ELEZZ
+\033[1;32m
+--------------------------------------------------
+"""
+User = """
+\033[1;96m ------------------------
+ \033[1;32m < COD BY SIDRA ELEZZ >
+ \033[1;96m ------------------------
+\033[1;91m ______   _______ _________
+\033[1;92m(  ___ \ / ___   )\__   __/
+\033[1;91m| (   ) )\/   )  |   ) (   
+\033[1;92m| (__/ /     /   )   | |   
+\033[1;91m|  __ (     /   /    | |   
+\033[1;92m| (  \ \   /   /     | |   
+\033[1;91m| )___) ) /   (_/\___) (___
+\033[1;92m|/ \___/ (_______/\_______/\033[1;93m.TET: @TT_RQ
+\033[1;91m
+
+     ° \033[1;95mTOOL USERNAME INSTA  CHACKER\033[1;91m °
+         
+\033[1;97m--------------------------------------------------
+\033[1;95m
+ AUTHOR     : SIDRA ELEZZ
+ Telegram   : TERMUX TOOLS
+ YOUTUBE    : TERMUX TOOLS
+ GITHUB     : GITHUB.COM/SIDRA ELEZZ
+\033[1;97m
+--------------------------------------------------
+"""
+ELEzz = """
+\033[1;96m ------------------------
+ \033[1;32m < COD BY SIDRA ELEZZ >
+ \033[1;96m ------------------------
+\033[1;91m ______   _______ _________
+\033[1;92m(  ___ \ / ___   )\__   __/
+\033[1;91m| (   ) )\/   )  |   ) (   
+\033[1;92m| (__/ /     /   )   | |   
+\033[1;91m|  __ (     /   /    | |   
+\033[1;92m| (  \ \   /   /     | |   
+\033[1;91m| )___) ) /   (_/\___) (___
+\033[1;92m|/ \___/ (_______/\_______/\033[1;93m.TET: @TT_RQ
+\033[1;91m
+
+     ° \033[1;95mTOOL USERNAME INSTA  CHACKER\033[1;91m °
+         
+\033[1;97m--------------------------------------------------
+"""
+#*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*#
+
+def wa():
+    webbrowser.open('https://t.me/TT_RQ/1')
+
+
+def restart():
+    ngulang = sys.executable
+    os.execl(ngulang, ngulang, *sys.argv)
+os.system('clear')
+print(Sidra)
+user = input('          \033[1;94mTOOL USERNAME: ')
+sandi = input('          \033[1;93mTOOL PASSWORD: ')
+if sandi == 'USER' and user == 'USER':
+    print( 'Anda Telah Login')
+    sys.exit
+else:
+    print(E+ " FIRST STEP Is Done. Logged in Successfully as "+H+ sandi)
+    wa()
+    restart()
+os.system('clear')
+print(Sidra)
+TK = input(E+"{"+A+"+"+E+"}"+H+ "Enter ENTER THE TOKN BOT :\n"+C)
+ID = input(E+"{"+A+"+"+E+"}"+H+ "Enter ENTER THE ID YOUR :\n"+C)
+os.system('clear')
+webbrowser.open('https://t.me/TT_RQ/1')
+headers = {
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'ar,en-US;q=0.9,en;q=0.8,ar-SA;q=0.7',
+                'content-length': '61',
+                'content-type': 'application/x-www-form-urlencoded',
+                'cookie': 'ig_cb=2; ig_did=BB52B198-B05A-424E-BA07-B15F3D4C3893; mid=YAlcaQALAAHzmX6nvD8dWMRVYFCO; shbid=15012; rur=PRN; shbts=1612894029.7666144; csrftoken=CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+                'origin': 'https://www.instagram.com',
+                'referer': 'https://www.instagram.com/accounts/emailsignup/',
+                'sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': generate_user_agent(),
+                'x-csrftoken': 'CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+                'x-ig-app-id': '936619743392459',
+                'x-ig-www-claim': 'hmac.AR0Plwj5om112fwzrrYnMNjMLPnyWfFFq1tG7MCcMv5_vN9M',
+                'x-instagram-ajax': '72bda6b1d047',
+                'x-requested-with': 'XMLHttpRequest'
+
+            } 
+
+def ketik(s):
+	for ASU in s + '\n':
+		sys.stdout.write(ASU)
+		sys.stdout.flush()
+		sleep(50. / 700)
+def error():
+	while True:
+		print('')		
+		print('خطا في لمعلومات')
+		print('error !! ')
+		
+os.system('clear')
+ketik(User)
+print (E+"{"+A+"1"+E+"}"+H+ "START CHECKER ")
+print (E+"{"+A+"0"+E+"}"+A+ "EXIT ")
+print (A)
+sidra = input(E+"{"+A+"?"+E+"}"+C+ "CHOOSE AN OPTION : "+C)
+
+tuks1 = 'poiuytrewqasdfghjklmnbvcxz1234567890'
+ru = ('{"account_created": false, "errors": {"email": [{"message": "Too many accounts are using a@gmail.com.", "code": "email_sharing_limit"}], "__all__": [{"message": "Create a password at least 6 characters long.", "code": "too_short_password"}]}, "dryrun_passed": false, "username_suggestions": [], "status": "ok", "error_type": "form_validation_error"}')
+os.system('clear')
+file = open(".txt","r").readlines() 
+if sidra == '1':
+	
+	Ok = 0
+	No = 0
+	Bo = 0
+	Number = 0
+	while True:
+		file1 = random.choice(file)
+		pw =str("".join(random.choice(tuks1)for i in range(2)))
+		us=(file1[0:-1]+"__"+pw)
+		
+		
+		
+		
+		url = 'https://www.instagram.com/accounts/web_create_ajax/attempt/'
+		
+		data = {
+		       'email' : 'a@gmail.com',
+                'username': f'{us}',
+                'first_name': 'AA',
+                'opt_into_one_tap': 'false'
+            }
+		SidraELEzz = requests.post(url,headers=headers,data=data).text
+		
+		
+		
+		if ru in  SidraELEzz:
+			os.system("clear")
+			print (ELEzz)
+			Number+= 1
+			Ok+= 1   
+			Bo+= 1
+			print(C+'USERNAME CHACKER ... ')
+			print("")
+			print(A+'PLEAS WAIT ... ')
+			print(C+"____________")
+			print(E+"°")
+			print(E+                       "°"+E+"         | OK LIVE USER :"+E+"{}".format(Ok))
+			print(E+                       "°"+C+"         | NO LIVE USER :"+A+"{}".format(No))
+			print(E+                       "°"+L+"         | SEND THE BOT :"+E+"{}".format(Bo))
+			print(E+"°")
+			print(E+                       "°"+H+"         | STERT CHACKE :"+H+"{}".format(Number))
+			tlg = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text=BY → Sidra ELEzz  ✓\n————————————\n .❖.User Instagram : [ → {} ← ]\n————————————\n .❖.𝐅𝐫𝐎𝐦 :  @TT_RQ  '.format(TK,ID,us)
+			requests.post(tlg)
+			with open("LIVE_USER.txt",'a') as ff:
+				ff.write(f"[✓] OK LIVE USER : {us}\n.❖.TELE: @TT_RQ\n\n")
+		else:
+			os.system("clear")
+			print (ELEzz)
+			Number+= 1
+			No+= 1
+			print(C+'USERNAME CHACKER ... ')
+			print("")
+			print(A+'PLEAS WAIT ... ')
+			print(C+"____________")
+			print(E+"°")
+			print(E+                       "°"+E+"         | OK LIVE USER :"+E+"{}".format(Ok))
+			print(E+                       "°"+C+"         | NO LIVE USER :"+A+"{}".format(No))
+			print(E+                       "°"+L+"         | SEND THE BOT :"+E+"{}".format(Bo))
+			print(E+"°")
+			print(E+                       "°"+H+"         | STERT CHACKE :"+H+"{}".format(Number))
+
+
