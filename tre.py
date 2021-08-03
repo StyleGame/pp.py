8f40f2045a48ab28c71726ffee1f925d93774385
StyleGame <69700134+StyleGame@users.noreply.github.com>
2021-08-03 03:04:25 +0200

    Create tre.py

---
+++ tre.py
@@ -0,0 +1,207 @@
+import requests
+import webbrowser
+import sys,os,time
+from time import sleep
+os.system("clear")
+A = "\033[1;91m"
+B = "\033[1;90m"
+C = "\033[1;97m"
+E = "\033[1;92m"
+H = "\033[1;93m"
+K = "\033[1;94m"
+L = "\033[1;95m"
+Sidra ="""
+ \033[1;96m ------------------------
+ \033[1;32m < COD BY SIDRA ELEZZ >
+ \033[1;96m ------------------------
+\033[1;91m ____  _     _
+\033[1;92m/ ___|(_) __| |_ __ __ _
+\033[1;91m\___ \| |/ _` | '__/ _` |
+\033[1;92m ___) | | (_| | | | (_| |
+\033[1;91m|____/|_|\__,_|_|  \__,_|
+           \033[1;93m _____ _     _____
+           \033[1;92m| ____| |   | ____|________
+           \033[1;91m|  _| | |   |  _| |_  /_  /
+           \033[1;92m| |___| |___| |___ / / / /
+           \033[1;91m|_____|_____|_____/___/___|\033[1;92m @TT_RQ
+           
+\033[1;97m--------------------------------------------------
+\033[1;95m
+ AUTHOR     : SIDRA ELEZZ
+ Telegram   : TERMUX TOOLS
+ YOUTUBE    : TERMUX TOOLS
+ GITHUB     : GITHUB.COM/SIDRA ELEZZ
+\033[1;32m
+--------------------------------------------------
+"""
+User = """
+\033[1;96m ------------------------
+ \033[1;32m < COD BY SIDRA ELEZZ >
+ \033[1;96m ------------------------
+\033[1;91m ______   _______ _________
+\033[1;92m(  ___ \ / ___   )\__   __/
+\033[1;91m| (   ) )\/   )  |   ) (   
+\033[1;92m| (__/ /     /   )   | |   
+\033[1;91m|  __ (     /   /    | |   
+\033[1;92m| (  \ \   /   /     | |   
+\033[1;91m| )___) ) /   (_/\___) (___
+\033[1;92m|/ \___/ (_______/\_______/\033[1;93m.TET: @TT_RQ
+\033[1;91m
+
+     ° \033[1;95mTOOL USERNAME INSTA  CHACKER\033[1;91m °
+         
+\033[1;97m--------------------------------------------------
+\033[1;95m
+ AUTHOR     : SIDRA ELEZZ
+ Telegram   : TERMUX TOOLS
+ YOUTUBE    : TERMUX TOOLS
+ GITHUB     : GITHUB.COM/SIDRA ELEZZ
+\033[1;97m
+--------------------------------------------------
+"""
+ELEzz = """
+\033[1;96m ------------------------
+ \033[1;32m < COD BY SIDRA ELEZZ >
+ \033[1;96m ------------------------
+\033[1;91m ______   _______ _________
+\033[1;92m(  ___ \ / ___   )\__   __/
+\033[1;91m| (   ) )\/   )  |   ) (   
+\033[1;92m| (__/ /     /   )   | |   
+\033[1;91m|  __ (     /   /    | |   
+\033[1;92m| (  \ \   /   /     | |   
+\033[1;91m| )___) ) /   (_/\___) (___
+\033[1;92m|/ \___/ (_______/\_______/\033[1;93m.TET: @TT_RQ
+\033[1;91m
+
+     ° \033[1;95mTOOL USERNAME INSTA  CHACKER\033[1;91m °
+         
+\033[1;97m--------------------------------------------------
+"""
+def Top(s):
+	for ASU in s + '\n':
+		sys.stdout.write(ASU)
+		sys.stdout.flush()
+		sleep(50. / 700)
+		
+re = requests.get("https://pastebin.com/raw/KKbLu0cb")
+print (Sidra)
+print ("\033[1;97mFIRST STEP OF LOGIN")
+print ("\033[1;97m--------------------")
+print ("\033[1;97m ") 
+password = input('          \033[1;93mTOOL PASSWORD: '+C)
+print (E)
+if password == "" :
+  sys.exit()
+if password in str(re.text):
+  Top(H+" FIRST STEP Is Done. Logged in Successfully as ")
+  print ("\033[1;93m ")
+  Top("\033[1;93mPlease Wait 5 Minutes, All Packages Are Checking.....")
+else:
+  print (" Wrong Password !")
+  webbrowser.open('https://t.me/TT_RQ/1')
+  sys.exit()
+os.system("clear")
+import requests
+import uuid
+import sys
+import os
+from random import *
+import colorama 
+from colorama import *
+import time 
+import string 
+from time import sleep
+from user_agent import generate_user_agent
+import random
+import webbrowser
+import requests
+r = requests.session()
+
+os.system('clear')
+print(ELEzz)
+token = input(E+"{"+A+"+"+E+"}"+H+ "Enter ENTER THE TOKN BOT :\n"+C)
+ID = input(E+"{"+A+"+"+E+"}"+H+ "Enter ENTER THE ID YOUR :\n"+C)
+start_msg = r.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=START HACK...🔥").json()
+id_msg	=(start_msg['result']["message_id"])
+os.system('clear')
+webbrowser.open('https://t.me/TT_RQ/1')
+headers = {
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'ar,en-US;q=0.9,en;q=0.8,ar-SA;q=0.7',
+                'content-length': '61',
+                'content-type': 'application/x-www-form-urlencoded',
+                'cookie': 'ig_cb=2; ig_did=BB52B198-B05A-424E-BA07-B15F3D4C3893; mid=YAlcaQALAAHzmX6nvD8dWMRVYFCO; shbid=15012; rur=PRN; shbts=1612894029.7666144; csrftoken=CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+                'origin': 'https://www.instagram.com',
+                'referer': 'https://www.instagram.com/accounts/emailsignup/',
+                'sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': generate_user_agent(),
+                'x-csrftoken': 'CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+                'x-ig-app-id': '936619743392459',
+                'x-ig-www-claim': 'hmac.AR0Plwj5om112fwzrrYnMNjMLPnyWfFFq1tG7MCcMv5_vN9M',
+                'x-instagram-ajax': '72bda6b1d047',
+                'x-requested-with': 'XMLHttpRequest'
+
+            } 
+
+
+os.system('clear')
+Top(User)
+print (E+"{"+A+"1"+E+"}"+H+ "START CHECKER ")
+print (E+"{"+A+"0"+E+"}"+A+ "EXIT ")
+print (A)
+sidra = input(E+"{"+A+"?"+E+"}"+C+ "CHOOSE AN OPTION : "+C)
+
+tuks1 = 'poiuytrewqasdfghjklmnbvcxz1234567890'
+ru = ('{"account_created": false, "errors": {"email": [{"message": "Too many accounts are using a@gmail.com.", "code": "email_sharing_limit"}], "__all__": [{"message": "Create a password at least 6 characters long.", "code": "too_short_password"}]}, "dryrun_passed": false, "username_suggestions": [], "status": "ok", "error_type": "form_validation_error"}')
+os.system('clear')
+file = open(".txt","r").readlines() 
+if sidra == '1':
+	
+	Ok = 0
+	Np = 0
+	Eo = 0
+	Number = 0
+	while True:
+		file1 = random.choice(file)
+		pw =str("".join(random.choice(tuks1)for i in range(2)))
+		us=(file1[0:-1]+"_"+pw)
+		
+		
+		
+		
+		url = 'https://www.instagram.com/accounts/web_create_ajax/attempt/'
+		
+		data = {
+		       'email' : 'a@gmail.com',
+                'username': f'{us}',
+                'first_name': 'AA',
+                'opt_into_one_tap': 'false'
+            }
+		SidraELEzz = requests.post(url,headers=headers,data=data).text
+		
+		
+		
+		if ru in  SidraELEzz:
+			os.system("clear")
+			print (ELEzz)
+			Np+= 1
+			Ok+= 1   
+			Eo+= 1
+			r.post(f"https://api.telegram.org/bot{token}/editmessagetext?chat_id={ID}&message_id={id_msg}&text=✰︎ Welcome Sidra ELEzz 👩‍💻  ⁦✰︎\n-----------------------------------------\n.✥. Available 💯 : {Ok}\n\n.✥. Unavailable ❌ : {Eo}\n\n-----------------------------------------\n.✥. STERT CHACKER 🔥: {Np}\n-----------------------------------------\n\n.✥. User Instagram  : [ → {us} ← ]\n\n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1")
+			r.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=✰︎ Hi Sidra Available User ✓ ⁦✰︎\n-----------------------------------------\n\n.✥. User Instagram : [ → {us} ← ] \n\n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1")
+			with open("LIVE_USER.txt",'a') as ff:
+				ff.write(f"[✓] OK LIVE USER : {us}\n.❖.TELE: @TT_RQ\n\n")
+		else:
+			os.system("clear")
+			print (ELEzz)
+			Np+= 1
+			Eo+= 1
+			r.post(f"https://api.telegram.org/bot{token}/editmessagetext?chat_id={ID}&message_id={id_msg}&text=✰︎ Welcome Sidra ELEzz 👩‍💻  ⁦✰︎\n-----------------------------------------\n.✥. Available 💯 : {Ok}\n\n.✥. Unavailable ❌ : {Eo}\n\n-----------------------------------------\n.✥. STERT CHACKER 🔥: {Np}\n-----------------------------------------\n\n.✥. User Instagram  : [ → {us} ← ]\n\n-----------------------------------------\n.✥.CH : @TT_RQ Tele: @SS_SS_1")
+			
+
+
